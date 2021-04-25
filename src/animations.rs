eaaae7f35cc1d97eb2e0a3ac9b2450634c033c27
Tristram Gräbener <tristramg@gmail.com>
2021-04-25 13:50:41 +0200

    Use rtic and cleanup

---
+++ src/animations.rs
@@ -0,0 +1,19 @@
+use micromath::F32Ext;
+use smart_leds::RGB8;
+
+pub fn tixy(t: f32, i: usize, x: usize, y: usize) -> RGB8 {
+    render(t.sin())
+}
+
+fn render(mut val: f32) -> RGB8 {
+    if val >= 1.0 {
+        val = 1.0
+    } else if val <= -1.0 {
+        val = -1.0;
+    }
+    if val < 0.0 {
+        RGB8::new((val * val * 100.0) as u8, 0, 0)
+    } else {
+        RGB8::new(0, (val * val * 100.0) as u8, 0)
+    }
+}
